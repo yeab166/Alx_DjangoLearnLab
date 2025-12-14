4e66f3680d4c7460b633837767676bc21ac96615
Yeabsira Belete <yeababa16@gmail.com>
2025-12-14 22:42:22 +0300

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,24 +1,20 @@
 from rest_framework import serializers
 from .models import Post, Comment
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source="author.username", read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = ["id", "post", "author", "author_username", "content", "created_at", "updated_at"]
-        read_only_fields = ["author", "created_at", "updated_at"]
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author_username = serializers.CharField(source="author.username", read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
+    author_username = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Post
-        fields = ["id", "author", "author_username", "title", "content", "created_at", "updated_at", "comments"]
-        read_only_fields = ["author", "created_at", "updated_at"]
+        fields = ['id', 'author', 'author_username', 'title', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['author']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['author']
