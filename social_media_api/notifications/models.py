78f2db25d070beb5d21e2ea18455e7cdf8916f47
Yeabsira Belete <yeababa16@gmail.com>
2025-12-14 22:29:56 +0300

    Update models.py

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,28 +1,20 @@
 from django.db import models
-from django.conf import settings
+from accounts.models import CustomUser
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
-User = settings.AUTH_USER_MODEL
-
 
 class Notification(models.Model):
-    recipient = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="notifications"
-    )
-    actor = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="actor_notifications"
-    )
+    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='actions')    
     verb = models.CharField(max_length=255)
 
-    target_content_type = models.ForeignKey(
-        ContentType, on_delete=models.CASCADE, null=True, blank=True
-    )
-    target_object_id = models.PositiveIntegerField(null=True, blank=True)
-    target = GenericForeignKey("target_content_type", "target_object_id")
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
 
-    is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.actor} {self.verb} {self.recipient}"
+        return f"{self.actor} {self.verb} {self.target}"
