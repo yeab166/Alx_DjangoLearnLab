eef6bc86f4768eb73924c37c73be9a42fb565f8c
Yeabsira Belete <yeababa16@gmail.com>
2025-12-14 22:32:22 +0300

    Update views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,13 +1,30 @@
-from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
 from .models import Notification
 from .serializers import NotificationSerializer
 
 
-class NotificationListView(generics.ListAPIView):
-    serializer_class = NotificationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class NotificationListView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        return Notification.objects.filter(
-            recipient=self.request.user
-        ).order_by("-timestamp")
+    def get(self, request):
+        notifications = Notification.objects.filter(recipient=request.user).order_by('is_read', '-timestamp')
+
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(serializer.data)
+
+
+class MarkNotificationReadView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        notification = Notification.objects.filter(id=pk, recipient=request.user).first()
+        if not notification:
+            return Response({"detail": "Not found"},status=status.HTTP_404_NOT_FOUND)
+
+        notification.is_read = True
+        notification.save()
+        return Response({"detail": "Marked as read"})
