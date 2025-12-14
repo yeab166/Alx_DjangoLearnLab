6a8dd5d2b0af9195bb25e5a517a988ae548b74d0
Yeabsira Belete <yeababa16@gmail.com>
2025-12-14 22:31:52 +0300

    Update utils.py

--- social_media_api/notifications/utils.py
+++ social_media_api/notifications/utils.py
@@ -1,16 +1,11 @@
 from django.contrib.contenttypes.models import ContentType
 from .models import Notification
 
-
-def create_notification(recipient, actor, verb, target=None):
-    notification = Notification(
+def create_notification(*, recipient, actor, verb, target):
+    Notification.objects.create(
         recipient=recipient,
         actor=actor,
-        verb=verb
+        verb=verb,
+        target_content_type=ContentType.objects.get_for_model(target),
+        target_object_id=target.id
     )
-
-    if target:
-        notification.target_content_type = ContentType.objects.get_for_model(target)
-        notification.target_object_id = target.id
-
-    notification.save()
