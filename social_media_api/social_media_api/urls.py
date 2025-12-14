4eac8a34a9b837d7c3e96e1a5894def904dc37c7
Yeabsira Belete <yeababa16@gmail.com>
2025-12-14 22:46:40 +0300

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -2,7 +2,7 @@
 URL configuration for social_media_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/posts/", include("posts.urls")),
-    path("api/accounts/", include("accounts.urls")),
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
+    path('notification/', include('notification.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
